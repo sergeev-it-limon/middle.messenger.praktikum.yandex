875169ae7d9fa52e544aa1cdbfd4c7b116676617
sergeev-it-limon <sergeev-it-limon@yandex.ru>
2021-11-14 17:28:57 +0500

    Added name to profileContent

--- src/newTs/components/profileContent/ProfileContent.ts
+++ src/newTs/components/profileContent/ProfileContent.ts
@@ -1,6 +1,7 @@
 import { htmlFromStr } from "../../utils/htmlFrom";
 import { BaseComponent, TChildren } from "../baseComponent";
 import { FormCommon } from "../formCommon/FormCommon";
+import { PageHeader } from "../pageHeader";
 import { ProfileAvatar } from "../profileAvatar";
 import "./profileContent.css";
 import style from "./profileContent.css.json";
@@ -15,11 +16,14 @@ export class ProfileContent extends BaseComponent {
 		const content = new FormCommon({ formClassName: style.root });
 
 		const profileAvatar = new ProfileAvatar(null);
+		const pageHeader = new PageHeader({ text: "Иван" });
 
 		profileAvatar.build(null);
+		pageHeader.build(null);
 
 		const top = document.createDocumentFragment();
 		top.appendChild(profileAvatar.ref);
+		top.appendChild(pageHeader.ref);
 
 		content.build({
 			top,
