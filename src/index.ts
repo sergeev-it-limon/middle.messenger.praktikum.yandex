3d8dad588921e32c45ff88771fc0c8a60d5bff87
sergeev-it-limon <sergeev-it-limon@yandex.ru>
2021-12-19 20:01:09 +0300

    Used Router

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "./common/commonStyles.css";
+import { Router } from "./controllers/Router";
 import { AuthPage } from "./pages/authPage";
 import { Error404Page } from "./pages/error404Page";
 import { Error500Page } from "./pages/error500Page";
@@ -6,44 +7,15 @@ import { HomePage } from "./pages/homePage";
 import { ProfilePage } from "./pages/profilePage";
 import { SelectedChatPage } from "./pages/selectedChatPage";
 import { SignupPage } from "./pages/signupPage";
+import { buildGetComponent } from "./utils/buildgetComponent";
 
-const root = document.getElementById("root");
-
-if (root == null) {
-	throw Error("Not found element with id root");
-}
-
-const pathname = window.location.pathname;
-let componentNode: HTMLElement;
-
-if (pathname === "/home") {
-	const homePage = new HomePage(null);
-	homePage.build(null);
-	componentNode = homePage.ref;
-} else if (/^\/selectedChat\/\d+$/.test(pathname)) {
-	const selectedChatPage = new SelectedChatPage(null);
-	selectedChatPage.build(null);
-	componentNode = selectedChatPage.ref;
-} else if (pathname === "/") {
-	const authPage = new AuthPage(null);
-	authPage.build(null);
-	componentNode = authPage.ref;
-} else if (pathname === "/profile") {
-	const profilePage = new ProfilePage(null);
-	profilePage.build(null);
-	componentNode = profilePage.ref;
-} else if (pathname === "/signup") {
-	const signupPage = new SignupPage(null);
-	signupPage.build(null);
-	componentNode = signupPage.ref;
-} else if (pathname === "/500") {
-	const error500Page = new Error500Page(null);
-	error500Page.build(null);
-	componentNode = error500Page.ref;
-} else {
-	const error404Page = new Error404Page(null);
-	error404Page.build(null);
-	componentNode = error404Page.ref;
-}
-
-root.appendChild(componentNode);
+const router = new Router("#root");
+router
+	.use("/home", buildGetComponent(new HomePage(null)))
+	.use("/selectedChat/:chatId", buildGetComponent(new SelectedChatPage(null)))
+	.use("/profile", buildGetComponent(new ProfilePage(null)))
+	.use("/500", buildGetComponent(new Error500Page(null)))
+	.use("/signup", buildGetComponent(new SignupPage(null)))
+	.use("/", buildGetComponent(new AuthPage(null)))
+	.use("*", buildGetComponent(new Error404Page(null)))
+	.start();
